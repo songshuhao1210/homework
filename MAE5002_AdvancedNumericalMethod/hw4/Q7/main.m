c75b200925af297106d1323d1c89f20d6b94021d
songshuhao1210 <2947438278@qq.com>
2023-04-07 23:41:06 +0800

    revise for hw4

--- MAE5002_AdvancedNumericalMethod/hw4/Q7/main.m
+++ MAE5002_AdvancedNumericalMethod/hw4/Q7/main.m
@@ -3,7 +3,7 @@ a = 0.5;
 n = 100;
 
 err_flag = 1e-3;
-y0 = ones(n-1,1);
+y0 = linspace(0,1,n-1)';
 
 [A,b] = gen_mat(eps,a,n);
 
@@ -11,13 +11,14 @@ y0 = ones(n-1,1);
 
 [y_gauss,flag_iter_gauss,err_gauss] = Gauss_iter(A,b,y0,err_flag);
 
-y0 = ana_func(eps,a,n);
+y0 = ana_func(eps,a,n)';
 
 x = linspace(0,1,n+1);
 x = x(2:end-1);
 
 plot(x,y0,'black--')
 hold on
-plot(x,y_jac,'r')
+plot(x,y_jac,'black*')
 plot(x,y_gauss,'g')
 legend({'Analytical solution','Jacobi','Gauss-Seidel'},'Location','northwest')
+hold off
